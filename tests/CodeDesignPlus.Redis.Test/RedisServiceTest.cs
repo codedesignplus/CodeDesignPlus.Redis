407ecb84c63d9f14933e2e007419e06622719f72
CodeDesignPlus <codedesignplus@outlook.com>
2022-02-03 14:41:12 -0500

    Update store install ca

--- tests/CodeDesignPlus.Redis.Test/RedisServiceTest.cs
+++ tests/CodeDesignPlus.Redis.Test/RedisServiceTest.cs
@@ -132,7 +132,7 @@ namespace CodeDesignPlus.Redis.Test
             if (!File.Exists(certificate))
                 throw new InvalidOperationException("Can't run unit test because certificate does not exist");
 
-            var store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+            var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
             store.Open(OpenFlags.ReadWrite);
             store.Add(new X509Certificate2(X509Certificate.CreateFromCertFile(certificate)));
             store.Close();
